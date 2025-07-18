8772dd91bbe7e4547519b1a998d831c191f013a6
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-07-18 20:14:19 +0700

    add: localstorage config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,1 +1,9 @@
 export const API_URL = process.env.BASE_API_URL || "http://127.0.0.1:8000";
+export const token =
+	typeof window !== "undefined" ? localStorage.getItem("token") || "" : "";
+
+export const isAuthenticated = !!token;
+export const userId =
+	typeof window !== "undefined" ? localStorage.getItem("userId") || "" : "";
+export const userName =
+	typeof window !== "undefined" ? localStorage.getItem("userName") || "" : "";
