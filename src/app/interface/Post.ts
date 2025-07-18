409cf1aec0e5ef38f51c6444f597406ca882927b
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-07-18 20:14:32 +0700

    up: interface post -> user

--- src/app/interface/Post.ts
+++ src/app/interface/Post.ts
@@ -1,14 +1,13 @@
 import { Comment } from "./Comment";
 import { Like } from "./Like";
+import { User } from "./User";
 
 export interface Post {
 	id: number;
 	image_path: string;
 	created_at: string;
 	caption: string | null;
-	user: {
-		name: string;
-	};
+	user: User;
 	likes: Like[];
 	likes_count: number;
 	is_liked?: boolean;
