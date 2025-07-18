51a78e1a51b90c6c2d3063917c36c13f79414f69
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-07-18 20:14:10 +0700

    add: config image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "127.0.0.1",
+				port: "8000",
+				pathname: "/api/image/posts/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
